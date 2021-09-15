62b9ec5e61b90b851038e9a3c23306d84637e502
Irina Bobova <irinabobova2005@gmail.com>
2021-09-15 22:19:29 +0700

    First commit

--- app/src/main/java/com/example/lab1helloworld/MainActivity.java
+++ app/src/main/java/com/example/lab1helloworld/MainActivity.java
@@ -9,9 +9,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        TextView text = (TextView) findViewById(R.id.text1);
-        text.setText("Hello World!");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+	TextView text = (TextView) findViewById(R.id.text1);
+        text.setText("Hello World!");
     }
 }
